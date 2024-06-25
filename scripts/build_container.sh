73f9b98643c7a092b03f00dab964068f49b4a596
Douglas Coburn <douglas@dactbc.com>
2024-06-24 23:10:00 -0700

    Updated build script to bypass building pypi artifacts on demand

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 VERSION=$(grep -o "__version__.*" socketsecurity/__init__.py | awk '{print $3}' | tr -d "'")
-
+BYPASS_PYPI_BUILD=$1
 echo $VERSION
-python -m build --wheel --sdist
-twine upload dist/*$VERSION*
-sleep 120
-docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:$VERSION . && docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:latest .
-docker push socketdev/cli:$VERSION && docker push socketdev/cli:latest
+
+if [ -z $BYPASS_PYPI_BUILD ] || [ $BYPASS_PYPI_BUILD -eq 0 ]; then
+  python -m build --wheel --sdist
+  twine upload dist/*$VERSION*
+  sleep 180
+fi
+docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:$VERSION . \
+&& docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:latest . \
+&& docker push socketdev/cli:$VERSION \
+&& docker push socketdev/cli:latest
