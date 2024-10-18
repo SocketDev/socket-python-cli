2ab577d0b0882da0cf2145a898f2091e0e029054
Douglas Coburn <douglas@dactbc.com>
2024-10-17 22:38:53 -0700

    Added support for CycloneDX and SPDX manifest files to the CLI. Also improved build script

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -5,32 +5,39 @@ STABLE_VERSION=$2
 echo $VERSION
 if [ -z $ENABLE_PYPI_BUILD ] || [ -z $STABLE_VERSION ]; then
   echo "$0 pypi-build=enable stable=true"
-  echo "\tpypi-build: Build and publish a new version of the package to pypi"
+  echo "\tpypi-build: Build and publish a new version of the package to pypi. Options are prod or test"
   echo "\tstable: Only build and publish a new version for the stable docker tag if it has been tested and going on the changelog"
   exit
 fi
 
-if [ $ENABLE_PYPI_BUILD = "pypi-build=enable" ]; then
+if [ $ENABLE_PYPI_BUILD = "pypi-build=prod" ]; then
   echo "Doing production build"
   python -m build --wheel --sdist
   twine upload dist/*$VERSION*
-  sleep 240
+  sleep 120
   docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:$VERSION . \
     && docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:latest . \
     && docker push socketdev/cli:$VERSION \
     && docker push socketdev/cli:latest
-  if [ $STABLE_VERSION = "stable=true" ]; then
-    docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:stable . \
-      && docker push socketdev/cli:stable
-  fi
-else
+fi
+
+if [ $ENABLE_PYPI_BUILD = "pypi-build=test" ]; then
   echo "Doing test build"
   python -m build --wheel --sdist
   twine upload --repository testpypi dist/*$VERSION*
-#  sleep 240
-#  docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:$VERSION . \
-#    && docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:latest . \
-#    && docker push socketdev/cli:$VERSION-test \
-#    && docker push socketdev/cli:test
+  sleep 120
+  docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:$VERSION . \
+    && docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:latest . \
+    && docker push socketdev/cli:$VERSION-test \
+    && docker push socketdev/cli:test
 fi
+
+
+if [ $STABLE_VERSION = "stable=true" ]; then
+    if [ $ENABLE_PYPI_BUILD = "pypi-build=enable" ]; then
+        sleep 120
+    fi
+    docker build --no-cache --build-arg CLI_VERSION=$VERSION --platform linux/amd64,linux/arm64 -t socketdev/cli:stable . \
+      && docker push socketdev/cli:stable
+  fi
 
